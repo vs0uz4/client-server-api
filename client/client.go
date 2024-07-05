3832121eb52e33b5b4a4e90ef60e3e084abed40b
vs0uz4 <vitor.rodrigues@gmail.com>
2024-07-05 08:41:04 -0300

    Fix context timeout time

--- client/client.go
+++ client/client.go
@@ -19,7 +19,7 @@ type FileInfo struct {
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
